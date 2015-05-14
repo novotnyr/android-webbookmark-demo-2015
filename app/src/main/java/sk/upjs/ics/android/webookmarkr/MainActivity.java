ebf8c8d89f5de3db366df507c96dd3e03561cae9
novotnyr <robert.novotny@upjs.sk>
2015-05-14 15:36:01 +0200

    removed layout from Main activity, as it will be wrapping the fragment

--- app/src/main/java/sk/upjs/ics/android/webookmarkr/MainActivity.java
+++ app/src/main/java/sk/upjs/ics/android/webookmarkr/MainActivity.java
@@ -1,5 +1,10 @@
 package sk.upjs.ics.android.webookmarkr;
 
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.provider.Browser;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,8 +16,6 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
-
 
 }
