ad684b60e9322d86c266f794ffd8e17e602a5df3
novotnyr <robert.novotny@upjs.sk>
2015-05-14 22:21:32 +0200

    added contextual action bar mode

--- app/src/main/java/sk/upjs/ics/android/webookmarkr/BookmarkListFragment.java
+++ app/src/main/java/sk/upjs/ics/android/webookmarkr/BookmarkListFragment.java
@@ -6,13 +6,19 @@ import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.BaseColumns;
 import android.provider.Browser;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.SimpleCursorAdapter;
 
-public class BookmarkListFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor> {
+public class BookmarkListFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor>, AbsListView.MultiChoiceModeListener {
 
-    private static final int BOOKMARK_LIST_LOADER = 0;
+    private static final int BOOKMARK_LIST_LOADER_ID = 0;
 
     private SimpleCursorAdapter listViewAdapter;
 
@@ -21,8 +27,14 @@ public class BookmarkListFragment extends ListFragment implements LoaderManager.
         super.onViewCreated(view, savedInstanceState);
 
         initListAdapter();
+        initList();
         setListAdapter(this.listViewAdapter);
-        getLoaderManager().initLoader(BOOKMARK_LIST_LOADER, Bundle.EMPTY, this);
+        getLoaderManager().initLoader(BOOKMARK_LIST_LOADER_ID, Bundle.EMPTY, this);
+    }
+
+    private void initList() {
+        getListView().setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE_MODAL);
+        getListView().setMultiChoiceModeListener(this);
     }
 
     private void initListAdapter() {
@@ -51,5 +63,50 @@ public class BookmarkListFragment extends ListFragment implements LoaderManager.
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
         this.listViewAdapter.swapCursor(Defaults.NO_CURSOR);
+    }
+
+    // ----- Fragment UI Logic
+
+    private void deleteSelectedListItems() {
+        for (long id : getListView().getCheckedItemIds()) {
+            getActivity().getContentResolver().delete(Browser.BOOKMARKS_URI, BaseColumns._ID + " = " + id, Defaults.NO_SELECTION_ARGS);
+            getLoaderManager().restartLoader(BOOKMARK_LIST_LOADER_ID, Bundle.EMPTY, this);
+        }
+    }
+
+    // ----- Contextual Action Bar callbacks
+
+    @Override
+    public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+        int checkedCount = getListView().getCheckedItemCount();
+        String checkedCountMessage = getResources().getQuantityString(R.plurals.selected_bookmarks, checkedCount, checkedCount);
+        mode.setSubtitle(checkedCountMessage);
+    }
+
+    @Override
+    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+        mode.getMenuInflater().inflate(R.menu.bookmarks_cab, menu);
+        mode.setTitle("Select Items");
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+        return false;
+    }
+
+    @Override
+    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.listViewDeleteSelectedAction:
+                deleteSelectedListItems();
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onDestroyActionMode(ActionMode mode) {
+        // do nothing
     }
 }
